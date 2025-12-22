21ed132c9852f480ec9ab2d66275281c8e6ed4cb
backslashN10 <205831853+backslashN10@users.noreply.github.com>
2025-12-22 19:24:49 +0100

    delete db backup funcionality

--- routerPOL/src/main/java/pl/edu/agh/po/Main.java
+++ routerPOL/src/main/java/pl/edu/agh/po/Main.java
@@ -3,9 +3,6 @@ package pl.edu.agh.po;
 
 public class Main {
     public static void main(String[] args) {
-        Utilities utilities = Utilities.getInstance();
-
         new Controller().start();
-        utilities.backupDatabase();
     }
 }
